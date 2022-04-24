bdee84b3e6f66a5a0a364cdb923c4bc1f1f5644a
Jeacy Espinoza <jeacyespinoza718@gmail.com>
2022-04-24 14:13:59 -0400

    added macros defining pins and some pin setup for range sensors

--- SkateboardSketch/SkateboardSketch.ino
+++ SkateboardSketch/SkateboardSketch.ino
@@ -1,9 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
+//========Macros=============
+//Ultrasonic Range Sensor Pin(s)
+#define PORTTRIGGER (10)
+#define STARTRIGGER (9)
+#define PORTECHO (13)
+#define STARTECHO (8)
 
+//Acceleromter + Gyroscope Pin(s)
+#define SCLPIN (A5)
+#define SDAPIN (A4)
+
+//LED Pin(s)
+#define LEDDATA (12)
+#define NUMLEDS (16)
+//========Globals=============
+//=========================
+void setup() {
+  //for serial monitor debugging
+  Serial.begin(9600);
+
+  //pin set up for ultrasonic range finders
+  pinMode(PORTTRIGGER, OUTPUT);
+  pinMode(STARTECHO, INPUT);
+  pinMode(STARTRIGGER, OUTPUT);
+  pinMode(PORTECHO, INPUT);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  ;
 }
+//=========================
