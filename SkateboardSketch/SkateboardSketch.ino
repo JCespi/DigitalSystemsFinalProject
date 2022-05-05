ce747ae97f2b6f4f707b3af9a9ca74e93bc1b3ef
Jeacy Espinoza <jeacyespinoza718@gmail.com>
2022-05-05 15:44:59 -0400

    got rid of everything but pixel related code

--- SkateboardSketch/SkateboardSketch.ino
+++ SkateboardSketch/SkateboardSketch.ino
@@ -7,51 +7,20 @@
 #include <Adafruit_GFX.h>       //OLED Display
 #include <Wire.h>
 //========Macros=============
-//Ultrasonic Range Sensor Pin(s)
-#define PORTTRIGGER (10)
-#define STARRIGGER (9)
-#define PORTECHO (13)
-#define STARECHO (8)
-
-//Acceleromter + Gyroscope Pin(s)
-#define SCLPIN (A5)
-#define SDAPIN (A4)
-
-//OLED Screen Pin(s)
-#define SCREENWIDTH (128)
-#define SCREENHEIGHT (64)
-// Declaration for SSD1306 display connected using software SPI (default case):
-#define OLEDMOSI (3)
-#define OLEDCLK (4)
-#define OLEDDC (5)
-#define OLEDCS (6)
-#define OLEDRESET (7)
-
 //LED Pin(s)
 #define LEDDATA (12)
 #define NUMPIXELS (16)
 #define DELAYVAL (500)  //time in ms to pause b/w pixels
 //constants defining the ranges of LED's on either side
 #define PORTSTART (0)
-#define PORTEND (0)
+#define PORTEND (14)
 #define STARSTART (0)
-#define STAREND (0)
+#define STAREND (14)
 //realistic distance values (should be 0 - 255) to change LED colors
 #define CLOSEST (10) //should be around 2 inches mapped to raw
 #define STEP (5)      //how much of a difference the value has to have to change color
 #define NSTEPS (9)  //how many colors are available
 //========Globals=============
-//Ultrasonic range variable(s)
-bool RangeError;
-
-//Acceleromter + Gyroscope variable(s)
-Adafruit_MPU6050 Mpu;
-
-//OLED Screen
-Adafruit_SSD1306 Display(SCREENWIDTH, SCREENHEIGHT,
-                                       OLEDMOSI, OLEDCLK, 
-                                       OLEDDC, OLEDRESET, OLEDCS);
-
 //LED Strip
 Adafruit_NeoPixel PixelStrip(NUMPIXELS, LEDDATA, 
                                            NEO_GRB + NEO_KHZ800);
@@ -71,16 +40,6 @@ void setup() {
   //for serial monitor debugging
   Serial.begin(9600);
 
-  //pin set up for ultrasonic range finders
-  initRangeSensors();
-
-  //initialize the accelerometer
-  initAccelerometer();
-  delay(100);
-
-  //initialize the OLED Screen
-  initDisplay();
-
   //initialize the LED strip/Neopixel
   initPixelStrip();
 }
@@ -88,24 +47,10 @@ void setup() {
 void loop() {
   float starDist, portDist;
   String debugMsg;
-  sensors_event_t a, g, temp;
 
   //----------------------------------------------------
-  //get the distance on the starboard and port sides of the board
-  portDist = getDistanceToSensor(true);
-  starDist= getDistanceToSensor(false);
-  
-  //check for an out-of-bounds range
-  debugMsg = "Port value = " + String(portDist) + " , starboard value = " + String(starDist);
-  printDebug(debugMsg);
-  if (RangeError)
-    return;
-  //----------------------------------------------------
-  //get the new sensor events
-  Mpu.getEvent(&a, &g, &temp);
-
-  //show the sensor event(s)
-  displayData(a, g, temp);
+  portDist = 10;
+  starDist = 10;
   //----------------------------------------------------
   //write to the pixel strip
   showPixelResponse(portDist, PORTSTART, PORTEND);
@@ -117,68 +62,8 @@ void printDebug(String msg){
   Serial.println("[DEBUG]" + msg);
 }
 
-void initRangeSensors(){
-  pinMode(PORTTRIGGER, OUTPUT);
-  pinMode(STARECHO, INPUT);
-  pinMode(STARRIGGER, OUTPUT);
-  pinMode(PORTECHO, INPUT);
-}
-
-void initAccelerometer(){
-  if (!Mpu.begin())
-    printDebug("Failed to find MPU6050 chip");
-
-  Mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
-  Mpu.setGyroRange(MPU6050_RANGE_250_DEG);
-  Mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-}
-
-void initDisplay(){
-  if (!Display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
-    printDebug("Failed to start display");
-
-  Display.display();
-  delay(500);
-  Display.setTextSize(1);
-  Display.setTextColor(WHITE);
-  Display.setRotation(0);
-}
-
 void initPixelStrip(){
   PixelStrip.begin();
-}
-
-unsigned int getDistanceToSensor(bool portSide){
-  float duration, distance;
-  unsigned int triggerPin, echoPin;
-
-  //assign the appropriate pins
-  triggerPin = (portSide == true) ? PORTTRIGGER : STARRIGGER;
-  echoPin = (portSide == true) ? PORTECHO : STARECHO;
-
-  digitalWrite(triggerPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(triggerPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(triggerPin, LOW);
-
-  duration = pulseIn(echoPin, HIGH);
-  distance = (duration / 2) * 0.0344;
-
-  if (distance >= 400 || distance <= 2){
-    RangeError = true;
-    printDebug("Raw distance value " + String(distance) + " out of range");
-  } else {
-    RangeError = false;
-
-    //convert to analog-friendly value
-    map(distance, 2, 400, 0, 255);
-    
-    delay(500);
-  }
-  delay(500);
-  
-  return distance;
 }
 
 void displayData(sensors_event_t a, sensors_event_t g, sensors_event_t temp){
